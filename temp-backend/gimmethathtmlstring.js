f0ee20680c74b4a5fdeb87f917f89d5cd19c8917
jubewe <ju.be.we@posteo.de>
2023-05-21 23:43:08 +0200

    fe-0.1.5

--- temp-backend/gimmethathtmlstring.js
+++ temp-backend/gimmethathtmlstring.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
 
-console.log(fs.readFileSync("../frontend/index.html", "utf-8").replace(/\t|\n(\s)*/g, ""));
+console.log(fs.readFileSync("../frontend/index.html", "utf-8").replace(/\t|\n(\s)*/g, " "));
