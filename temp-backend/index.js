8fd1681c33e2fe6df11f24c0cabfc84ab05ce764
jubewe <ju.be.we@posteo.de>
2023-05-30 12:03:29 +0200

    fe-0.2.1

--- temp-backend/index.js
+++ temp-backend/index.js
@@ -3,20 +3,6 @@ const html = express.static("./frontend");
 const { WebSocketServer } = require("ws");
 
 const app = express();
-const wsServer = new WebSocketServer({
-    host: "localhost",
-    port: 81
-});
-
-wsServer.on("listening", () => {
-    console.log("WS Server listening");
-});
-
-wsServer.on("connection", conn => {
-    conn.onmessage = (msg) => {
-        console.log("WS Message", msg.data);
-    };
-});
 
 app.listen(80, () => {
     console.log("express listening on port", 80);
@@ -28,4 +14,29 @@ app.post("/update", (req, res) => {
     console.log("Backend received POST update:", req.body, req.headers);
 
     res.status(200).send('{"message": "Success"}');
+});
+
+const wsServer = new WebSocketServer({
+    host: "localhost",
+    // server: app,
+    // path: "/ws",
+    port: 81
+});
+
+wsServer.addListener("listening", () => {
+    console.log("WS Server connected");
+});
+
+wsServer.addListener("connection", conn => {
+    conn.onmessage = (msg) => {
+        console.log("WS Message", msg.data);
+    };
+});
+
+wsServer.addListener("error", e => {
+    console.error(e)
+});
+
+wsServer.addListener("close", () => {
+    console.log("WS Server closed");
 });
