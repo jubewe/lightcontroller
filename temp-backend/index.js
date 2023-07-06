34b06586de2b780b5f3e930ae6b58733753f5eec
jubewe <ju.be.we@posteo.de>
2023-07-06 21:26:14 +0200

    fe-0.3.9

--- temp-backend/index.js
+++ temp-backend/index.js
@@ -5,38 +5,46 @@ const { WebSocketServer } = require("ws");
 const app = express();
 
 app.listen(80, () => {
-    console.log("express listening on port", 80);
+  console.log("express listening on port", 80);
 });
 
-app.use("/", html)
+app.use("/", html);
 
 app.post("/update", (req, res) => {
-    console.log("Backend received POST update:", req.body, req.headers);
+  console.log("Backend received POST update:", req.body, req.headers);
 
-    res.status(200).send('{"message": "Success"}');
+  res.status(200).send('{"message": "Success"}');
 });
 
 const wsServer = new WebSocketServer({
-    host: "localhost",
-    // server: app,
-    // path: "/ws",
-    port: 81
+  host: "localhost",
+  // server: app,
+  // path: "/ws",
+  port: 81,
 });
 
 wsServer.addListener("listening", () => {
-    console.log("WS Server connected");
+  console.log("WS Server connected");
 });
 
-wsServer.addListener("connection", conn => {
-    conn.onmessage = (msg) => {
-        console.log("WS Message", msg.data);
-    };
+let exampleStates = "1,10,20,30,40,50,60;19,120,130,140,150,160,170;7,120,130,140,150,160,170";
+
+wsServer.addListener("connection", (ws) => {
+  ws.send(`refresh;${exampleStates}`);
+
+  ws.onmessage = (msg) => {
+    console.log("WS Message", msg.data);
+
+    if (msg === "refresh") {
+      ws.send(`refresh;${exampleStates}`);
+    }
+  };
 });
 
-wsServer.addListener("error", e => {
-    console.error(e)
+wsServer.addListener("error", (e) => {
+  console.error(e);
 });
 
 wsServer.addListener("close", () => {
-    console.log("WS Server closed");
+  console.log("WS Server closed");
 });
